e47de9f031d6989a29ad72a1dd870246df7a1256
dethodav <derek.davis@ligo.org>
2016-10-07 11:05:04 -0400

    Update array_inputs.py

--- bin/array_inputs.py
+++ bin/array_inputs.py
@@ -15,14 +15,14 @@ if __name__ == '__main__':
   time_start = (time) - 5
   time_end = (time) + 5
 
-  item = 0
+  item = int(0)
   source = zeros(3)
 
   golden = ['L1:GDS-CALIB_STRAIN',1126593017,1126593037]
 
   while (item < len(time)):
 
-    source[0] = str(channel[item])
+    source[0] = channel[item]
     source[1] = time_start[item]
     source[2] = time_end[item]
     
