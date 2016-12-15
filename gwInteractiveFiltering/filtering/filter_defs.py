2a59bef1f0060c0b7d459706f428d5f013b83dae
dethodav <derek.davis@ligo.org>
2016-12-15 10:17:07 -0500

    Update filter_defs.py

--- gwInteractiveFiltering/filtering/filter_defs.py
+++ gwInteractiveFiltering/filtering/filter_defs.py
@@ -81,7 +81,7 @@ def freqshift(timeseries,fshift):
 	
 	freq_rep = np.fft.rfft(data)
 	shifted_freq = numpy.zeros(len(freq_rep))
-	for i in range(0,len(freq_rep):
+	for i in range(0,len(freq_rep)):
 		if 0<(i-nbins)<len(freq_rep):
 		       shifted_freq[i]=freq_rep[i-nbins]
 	out = np.fft.irfft(shifted_freq)	       
